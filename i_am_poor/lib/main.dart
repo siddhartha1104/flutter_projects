5d412d19031233da4ebea35d451698d487b4f7d0
Siddhartha Pathak <siddhartha.pathak1104@gmail.com>
2023-03-25 01:30:00 +0530

    2nd app

---
+++ i_am_poor/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('I am Poor'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/mountain.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
